5c9e90c60bdcf50667979032b81b0e25c1adf90e
Arif Hidayat <84622058+hiarif21@users.noreply.github.com>
2022-07-02 18:27:29 +0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <div>Tontonin-Frontend</div>;
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      default: [
+        'Inter',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Poppins',
+        'Roboto',
+        '"Droid Sans"',
+        'sans-serif',
+      ],
+    },
+  },
+  plugins: [],
+};
