3adf1ce84f451bbd86b640a4e60413377f0d2772
Arif Hidayat <84622058+hiarif21@users.noreply.github.com>
2022-07-03 09:50:31 +0700

    infinite scrolling

---
+++ components/molecules/commons/InfiniteScrolling/index.tsx
@@ -0,0 +1,24 @@
+import { useRef } from 'react';
+import useInfinite from '../../../../hooks/useInfinite';
+
+const InfiniteScrolling = ({
+  loadMore,
+  finished,
+  markFinished,
+}: InfiniteScrollingProps) => {
+  const ref = useRef(null);
+
+  useInfinite(ref, loadMore);
+
+  if (finished) {
+    return markFinished ? <span className="atext-xs">🙄 No More!</span> : null;
+  }
+
+  return (
+    <span ref={ref} className="animate-pulse text-xs">
+      🧐 Wait...
+    </span>
+  );
+};
+
+export default InfiniteScrolling;
