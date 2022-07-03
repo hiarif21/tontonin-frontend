0639e23135f508a532384fe53f168f28a31a134c
Arif Hidayat <84622058+hiarif21@users.noreply.github.com>
2022-07-03 09:49:22 +0700

    install dependencies and update env
    install axios usehooks-ts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const domain_image = process.env.NEXT_PUBLIC_DOMAIN_IMAGE.split(' ');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: domain_image,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
