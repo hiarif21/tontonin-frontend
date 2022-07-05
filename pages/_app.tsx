3c269676b0a789915ebc7cce99aec8e960ee74ab
Arif Hidayat <84622058+hiarif21@users.noreply.github.com>
2022-07-05 20:00:12 +0700

    add useScrollRestoration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { DiscoversProvider } from '../context/discovers';
 import { MoviesProvider } from '../context/movies';
+import useScrollRestoration from '../hooks/useScrollRestoration';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+  useScrollRestoration(router);
   return (
     <MoviesProvider>
       <DiscoversProvider>
